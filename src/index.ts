0ef5377128e6484cf6b9ece27409290533fe8ae5
Nikhil <kumarnikhil48578@gmail.com>
2025-09-22 23:45:42 +0530

    fix: cors error

--- src/index.ts
+++ src/index.ts
@@ -18,6 +18,7 @@ app.use(
     cors({
         origin: [
             "http://localhost:5173",
+            "https://mysearch.pixxelai.live"
         ],
         methods: ["GET", "POST", "PUT", "DELETE", "PATCH", "OPTIONS"],
         credentials: true,
